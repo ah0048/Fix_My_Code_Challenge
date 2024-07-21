fa2e19a38fc47739dde758035f1f70cfd4e1ba31
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-07-22 00:29:54 +0300

    edit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 
-class Square:
+class square():
+    
+    width = 0
+    height = 0
+
     
     def __init__(self, *args, **kwargs):
-        self.width = kwargs.get('width', 0)
-        self.height = kwargs.get('height', 0)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        if hasattr(self, 'width') and hasattr(self, 'height') and self.height != self.width:
+            raise ValueError('for a square, height must be the same as width')
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
-    def perimeter_of_my_square(self):
-        return self.width * 4
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.PermiterOfMySquare())
